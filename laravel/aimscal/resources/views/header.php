6fa3aa03f2f027a9003b1629a72be9eab661cdc6
emazeem <62165761+emazeem@users.noreply.github.com>
2024-01-05 11:54:03 +0500

    ds

---
+++ laravel/aimscal/resources/views/header.php
@@ -0,0 +1,25 @@
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/AIMS.png" style="width: 50px"  alt="">
+                    </a>
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                        <li class="scroll-to-section"><a href="#services">Services</a></li>
+                        <li class="scroll-to-section"><a href="#courses">Request a Quote</a></li>
+                        <li class="scroll-to-section"><a href="#team">Team</a></li>
+                        <li class="scroll-to-section"><a href="#events">About Us</a></li>
+                        <li class="scroll-to-section"><a href="#contact">Contact Us</a></li>
+                    </ul>
+                    <a class="menu-trigger">
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
