1954e040de0b908fb9e7c3fa3f3c31bd486ef8c7
Faizan Ahmad <faizan.ahmad@clustox.com>
2024-02-10 17:27:52 +0500

    added the rental equipment page in to the app and updated the sizes of the buttons in the app.

--- web/components/equipment-rental.php
+++ web/components/equipment-rental.php
@@ -1,36 +1,38 @@
 <?php
 $services = [
-    array('title' => 'MultiMeter', 'description' => 'Accredited and Traceable to ensure measurement comparability.', 'icon' => 'calibration.png', 'url' => 'services_calibration.php'),
-    array('title' => 'MultiMeter', 'description' => 'Accredited and Traceable to ensure measurement comparability.', 'icon' => 'inspection.png', 'url' => 'services_calibration.php'),
-    array('title' => 'MultiMeter', 'description' => 'Prepare you to meet international standards and maintain in-house capability for calibration.', 'icon' => 'training.png', 'url' => 'services_metrology_consultancy.php'),
-    array('title' => 'MultiMeter', 'description' => 'Prepare you to meet international standards and maintain in-house capability for calibration.', 'icon' => 'training.png', 'url' => 'services_qms_consultancy.php'),
-    array('title' => 'MultiMeter', 'description' => 'Ensure electrical safety from electrical appliances and reliability of medical diagnostics.', 'icon' => 'calibration.png', 'url' => 'services_e-and-i.php'),
-    array('title' => 'MultiMeter', 'description' => 'Accredited and Traceable to ensure measurement comparability.', 'icon' => 'lifting.png', 'url' => '#'),
-    array('title' => 'MultiMeter', 'description' => 'Avoid costly failures and industrial accidents.', 'icon' => 'calibration.png', 'url' => '#'),
-    array('title' => 'MultiMeter', 'description' => 'Accredited and Traceable to ensure measurement comparability.', 'icon' => 'tank.png', 'url' => '#'),
-    array('title' => 'MultiMeter', 'description' => 'Increase service life of your buried assets.', 'icon' => 'electrolysis.png', 'url' => '#'),
+    array('title' => 'MultiMeter', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'calibration.png', 'salePrice' => 'Rs 10,000', 'url' => 'services_calibration.php'),
+    array('title' => 'HPLC', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'inspection.png', 'salePrice' => 'Rs 10,000', 'url' => 'services_calibration.php'),
+    array('title' => 'Spectrophotometer', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'training.png', 'salePrice' => 'Rs 10,000', 'url' => 'services_metrology_consultancy.php'),
+    array('title' => 'pH Solution', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'training.png', 'salePrice' => 'Rs 10,000', 'url' => 'services_qms_consultancy.php'),
+    array('title' => 'Stop Watch', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'calibration.png', 'salePrice' => 'Rs 10,000', 'url' => 'services_e-and-i.php'),
+    array('title' => 'Lux Meter', 'make'=> 'Mettler','model' => 'AIMS-121', 'icon' => 'electrolysis.png', 'salePrice' => 'Rs 10,000', 'url' => '#'),
 ];
 
 ?>
 <div class="services section" id="services">
     <div class="container">
         <div class="row">
-            <?php foreach($services as $service){ ?>
-                <div class="col-lg-4 col-md-6">
-                    <div class="service-item">
-                        <div class="icon">
+          <?php foreach($services as $service){ ?>
+                <div class="col-lg-3 col-md-6 ">
+                    <div class=main-content>
+                    <div class="card mb-5" style="width: 17rem;">
+                    <div class="icon">
                             <img src="public/icons/<?php echo $service['icon'] ?>" alt="online degrees" />
                         </div>
-                        <div class="main-content">
-                            <h4><?php echo $service['title'] ?></h4>
-                            <p><?php echo $service['description'] ?></p>
+                        <div class="card-body">
+                        <h5><?php echo $service['title'] ?></h5>
+                            <p><strong>Make: </strong><?php echo $service['make'] ?></p>
+                            <p><strong>Model: </strong><?php echo $service['model'] ?></p>
+                            <p><strong>Sale Price: </strong><?php echo $service['salePrice'] ?></p>
                             <div class="main-button">
                                 <a href="<?php echo $service['url'] ?>">Read More</a>
                             </div>
                         </div>
                     </div>
+          </div>
                 </div>
             <?php } ?>
+        </div>
         </div>
     </div>
 </div>
